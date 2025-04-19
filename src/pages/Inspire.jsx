154c67053307ab1f3f02cdc476e5f3d83864b607
putrimail <syarowiputri@gmail.com>
2025-04-19 08:39:21 +0800

    update 19.5

--- src/pages/Inspire.jsx
+++ src/pages/Inspire.jsx
@@ -26,11 +26,19 @@ function Inspire() {
             <Option
               logo={<logos.meet />}
               text="ZOOM"
-              link="https://s.bps.go.id/inspire_ep5"
+              link="https://telkomsel.zoom.us/j/94903526876?pwd=RVh2ehMbYXxai46h4VBaMm1IuURF1A.1"
             />
-            <Option logo={<logos.cekin />} text="ABSENSI" link="" />
-            {/* <Option logo={<logos.serifikat />} text="SERTIFIKAT" link="" />
-            <Option logo={<logos.drive />} text="DOKUMENTASI" link="" /> */}
+            <Option
+              logo={<logos.cekin />}
+              text="ABSENSI"
+              link="https://webapps.bps.go.id/daftarhadir/#/go/cu9qa"
+            />
+            <Option
+              logo={<logos.serifikat />}
+              text="SERTIFIKAT"
+              link="https://drive.google.com/drive/folders/11jQ6RUVTIOSDSLaBMop3Pj_kPm9L_27N?usp=sharing"
+            />
+            {/* <Option logo={<logos.drive />} text="DOKUMENTASI" link="" /> */}
           </div>
         </div>
       </div>
